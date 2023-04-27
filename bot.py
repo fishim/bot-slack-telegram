5cdf8f23a4f8ed82bba09af64300dc62970a968a
maksym-karas <fishartcommercial@gmail.com>
2023-04-27 19:23:32 +0300

    get and send message

--- bot.py
+++ bot.py
@@ -14,25 +14,31 @@ class Message:
         self.is_it_group = is_it_group
 
 
-def get_message(cls):
-    if not funct.Check_user(cls.user_id):
-        funct.Insert_user(cls.user_id, cls.platform, cls.chat_id)
+# def get_message(cls):
+#     if not funct.Check_user(cls.user_id):
+#         funct.Insert_user(cls.user_id, cls.platform, cls.chat_id)
+#
+#     funct.Insert_messenge(
+#         cls.text, cls.user_id,
+#         datetime.fromtimestamp(cls.date),
+#         cls.user_name, cls.is_it_group
+#     )
+#     send_message(cls)
 
-    funct.Insert_messenge(
-        cls.text, cls.user_id,
-        datetime.fromtimestamp(cls.date),
-        cls.user_name, cls.is_it_group
-    )
-    send_message(cls)
+def get_message(chat_id, text, messenger):
+    thirt.echo(chat_id, text, messenger)
+
+def send_message(chat_id, text, messenger):
+    BotCode.set_message(chat_id, text)
 
 
-def send_message(cls):
-    try:
-        if cls.platform == 'TG':
-            return cls.chat_id, cls.text
-        elif cls.platform == 'SL':
-            return 'slack'
-    except:
-        print('Could not send message.')
-        print("Unexpected error:", sys.exc_info()[0])
-
+# def send_message(cls):
+#     try:
+#         if cls.platform == 'TG':
+#             return cls.chat_id, cls.text
+#         elif cls.platform == 'SL':
+#             return 'slack'
+#     except:
+#         print('Could not send message.')
+#         print("Unexpected error:", sys.exc_info()[0])
+#
